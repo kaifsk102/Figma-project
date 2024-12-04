b6ac9f020623a484dfc7474037a5dbbd753e4433
kaifsk102 <kaifsk102@gmail.com>
2024-12-04 21:50:53 +0530

    Your commit message

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -4,7 +4,7 @@ import DropdownMenu from './DropdownMenu'
 
 const Navbar = () => {
   return (
-    <div className=' flex justify-between text-center items-center px-24 py-10 text-[#FFFFFF] '>   
+    <div className=' container flex justify-between text-center items-center px-24 py-10 text-[#FFFFFF] '>   
       <p className='font-gilroy font-bold  text-[28px] leading-[32.81px] tracking-[0.01em] '>Panto</p>
       <div className='w-[478px] h-[22px]  font-gilroy font-medium text-[18px] leading-[21.09px]  space-x-16 '>
       <a href="#" className="hover:underline"><DropdownMenu/></a>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,15 +8,17 @@ import Materials from '../../Component/Materials'
 import Testimonials from '../../Component/Testimonials'
 import Footer from '../../Component/Footer'
 import BestSellingProduct from '../../Component/BestSellingProduct'
+import './Home.css'
 
 
 const Home = () => {
   return (
     <>
     <div>
-    <div className=' h-[1084px] bg-custom-gradient 'style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', width:'full', }}>
+    <div className=' h-[1084px] banner  '>
     <Navbar/>
     <div className='grid items-center justify-center '>
+    
       <h1 className='absolute top-[161px] left-[290px] w-[860px] h-[200px] font-gilroy font-bold text-[77px] leading-[104px] tracking-[-0.01em] text-[#FFFFFF] text-center'>Make Your Interior More Minimalistic & Modern</h1>
       <p className='absolute top-[383px] left-[418px] w-[606px] h-[76px]  opacity-[0.8] font-gilroy  text-[24px] leading-[38.4px] text-center text-[#FFFFFF]'>Turn your room with panto into a lot more minimalist and modern with ease and speed</p>
     <SearchBox/>
