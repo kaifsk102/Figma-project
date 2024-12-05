22b28461d15d939ea237ace846132a1effa169d2
kaifsk102 <kaifsk102@gmail.com>
2024-12-05 09:01:42 +0530

    commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <>
     <div className=' w-full h-full '>
-    <div className=' w-[1440px] mx-auto h-[1084px] bg-cover bg-custom-gradient '>
+    <div className=' w-[1440px] mx-auto h-[1084px] bg-cover bg-no-repeat bg-custom-gradient '>
     <Navbar/>
     <div className='grid items-center justify-center '>
       <h1 className='w-[861px] h-[208px] absolute top-[161px] left-[340px] font-gilroy font-bold text-[77px] leading-[104px] tracking-[-0.01em] text-[#FFFFFF] text-center'>Make Your Interior More Minimalistic & Modern</h1>
