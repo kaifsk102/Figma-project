64fe6e77e7457909b1072151d992ce5728d6f58f
kaifsk102 <kaifsk102@gmail.com>
2024-12-05 09:42:20 +0530

    Your commit message

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {
-      backgroundImage: { 'custom-gradient': ["linear-gradient(180deg, rgba(255, 255, 255, 0) 86.13%, #FFFFFF 100%), url('./bannaer.jpg')"], },      
       fontFamily: {  
-      'another-font': ['AnotherFont', 'sans-serif'],
       'gilroy': ['Gilroy', 'sans-serif'], 
       'inter': ['Inter', 'sans-serif'],
       },
