93affc4414691dce86114c4374859b892a4b6c84
MD KAIF SK <93993949+kaifsk102@users.noreply.github.com>
2024-12-05 09:21:00 +0530

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -12,13 +12,9 @@ import BestSellingProduct from '../../Component/BestSellingProduct'
 const Home = () => {
   return (
     <>
-      <style> {` .banner {  
-      background-image: url(./bannaer.jpg); 
-      background-size: cover;  
-      background-position: center; 
-      background-repeat: no-repeat; } `} </style>
+     
     <div className=' w-full h-full '>
-    <div className=' w-[1440px] mx-auto h-[1084px] banner ' >
+    <div className=' w-[1440px] mx-auto h-[1084px] ' >
     <Navbar/>
     <div className='grid items-center justify-center '>
       <h1 className='w-[861px] h-[208px] absolute top-[161px] left-[340px] font-gilroy font-bold text-[77px] leading-[104px] tracking-[-0.01em] text-[#FFFFFF] text-center'>Make Your Interior More Minimalistic & Modern</h1>
